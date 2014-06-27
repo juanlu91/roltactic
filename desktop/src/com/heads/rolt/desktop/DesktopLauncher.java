aeefde10e4d9154f7babf6764749b1c9f7fb0157
Juanlu de la Fuente <juanlu.delafuente7@gmail.com>
2014-06-27 19:48:55 +0200

    dimensiones corregidas

--- desktop/src/com/heads/rolt/desktop/DesktopLauncher.java
+++ desktop/src/com/heads/rolt/desktop/DesktopLauncher.java
@@ -6,7 +6,11 @@ import com.heads.rolt.MyGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGame(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "rol-tactics";
+        cfg.width = 800;
+        cfg.height = 480;
+        
+		new LwjglApplication(new MyGame(), cfg);
 	}
 }
